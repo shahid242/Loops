d76fe6caf801a137835bca8a4b665d264a1774cb
Shahid Ali <bajeershahid125@gmail.com>
2025-11-09 13:13:03 -0800

    loop-cpp

---
+++ loop1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while(i <= 6) {
+        cout << "Count" << i << endl;
+        i++;
+    }
+    return 0;
+}
---
+++ loop2.cpp
@@ -0,0 +1,12 @@
+// do...while loop
+#include <iostream>
+using namespace std;
+int main () {
+    int i = 1;
+    do {
+        cout <<"Value :" << i << endl;
+        i++;
+    } while (i <= 6);
+    return 0;
+    
+}
