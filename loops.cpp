01f24b2de7f8d181bafcda4d7d1eb0d055102c0c
Shahid Ali <bajeershahid125@gmail.com>
2025-11-09 12:35:16 -0800

    loops-cpp

---
+++ loops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //For Loop example
+    for(int i = 1; i <= 5; i++) {
+        cout << "Hello, C++!" << endl;
+    }
+    return 0;
+}
